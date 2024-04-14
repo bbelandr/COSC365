86f031da17fe86150ffcb80b58ee3397f6c0241a
bbelandr <bbelandr@vols.utk.edu>
2024-04-14 19:12:50 -0400

    Working? conversion to BCD

--- C#Zone/DPDLab/DPD.cs
+++ C#Zone/DPDLab/DPD.cs
@@ -2,6 +2,42 @@ using System.Text.RegularExpressions;
 
 class DPD : ICompValue {
     private uint _raw;
+    // private int[,] DPDBits = {  // The sub-arrays record where each letter is within the DPD-packed format. DPDBits[0,1] records where B is in the first row.
+    //     {9, 8, 7, 6, 5, 4, 2, 1, 0},
+    //     {9, 8, 7, 6, 5, 4, -1, -1, 0},  // in this case, -1 means that there is no location for g and h
+    //     {9, 8, 7, -1, -1, 4, 6, 5, 0},
+    //     {-1, -1, 7, 6, 5, 4, 9, 8, 0},
+    //     {-1, -1, 7, -1, -1, 4, 9, 8, 0},
+    //     {-1, -1, 7, 9, 8, 4, -1, -1, 0},
+    //     {9, 8 , 7, -1, -1, 4, -1, -1, 0},
+    //     {-1, -1, 7, -1, -1, 4, -1, -1, 0}
+
+    // };
+
+    private char[][] DPDBits = {
+        new char[] {'a', 'b', 'c', 'd', 'e', 'f', '0', 'g', 'h', 'i'},
+        new char[] {'a', 'b', 'c', 'd', 'e', 'f', '1', '0', '0', 'i'},
+        new char[] {'a', 'b', 'c', 'g', 'h', 'f', '1', '0', '1', 'i'},
+        new char[] {'g', 'h', 'c', 'd', 'e', 'f', '1', '1', '0', 'i'},
+        new char[] {'g', 'h', 'c', '0', '0', 'f', '1', '1', '1', 'i'},
+        new char[] {'d', 'e', 'c', '0', '1', 'f', '1', '1', '1', 'i'},
+        new char[] {'a', 'b', 'c', '1', '0', 'f', '1', '1', '1', 'i'},
+        new char[] {'0', '0', 'c', '1', '1', 'f', '1', '1', '1', 'i'}
+    };
+    private char[][] BCDBits = {
+        new char[] {'0', 'a', 'b', 'c', '0', 'd', 'e', 'f', '0', 'g', 'h', 'i'},
+        new char[] {'0', 'a', 'b', 'c', '0', 'd', 'e', 'f', '1', '0', '0', 'i'},
+        new char[] {'0', 'a', 'b', 'c', '1', '0', '0', 'f', '0', 'g', 'h', 'i'},
+        new char[] {'1', '0', '0', 'c', '0', 'd', 'e', 'f', '0', 'g', 'h', 'i'},
+        new char[] {'1', '0', '0', 'c', '1', '0', '0', 'f', '0', 'g', 'h', 'i'},
+        new char[] {'1', '0', '0', 'c', '0', 'd', 'e', 'f', '1', '0', '0', 'i'},
+        new char[] {'0', 'a', 'b', 'c', '1', '0', '0', 'f', '1', '0', '0', 'i'}, 	
+        new char[] {'1', '0', '0', 'c', '1', '0', '0', 'f', '1', '0', '0', 'i'}
+
+    };
+    public DPD() {
+        
+    }
 
     private bool IsBitSet(uint bits, int index) {
         return ((bits >> index) & 1) == 1;
@@ -9,6 +45,13 @@ class DPD : ICompValue {
     private void SetBit(ref uint bits, int index) {
         bits = bits | (uint)(1 << index);
     }
+    private void CopyBit(uint sourceBits, int sourceIndex, ref uint destBits, int destIndex) {
+        if (IsBitSet(sourceBits, sourceIndex)) {
+            SetBit(ref destBits, destIndex);
+        }
+    }
+
+    
     int IComparable.CompareTo(object? obj) {
         // TODO: Compare objects
         return -1;
@@ -33,7 +76,7 @@ class DPD : ICompValue {
             }
 
             // Find which row of the table corresponds with the current value
-            int[] rows = new int[3];
+            int[] rows = new int[3];    // Which row this group of bits belongs to
             for (int i = 0; i < bitGroups.Length; i++) {
                 if ((bitGroups[i] & 0b_000_000_1_000) == 0) {
                     rows[i] = 1;
@@ -72,19 +115,32 @@ class DPD : ICompValue {
                 
             }
 
-            uint ass = 1;
-            SetBit(ref ass, 2);
-            SetBit(ref ass, 1);
-            Console.WriteLine(ass);
-            // // Do the conversions to BCD
-            // for (int i = 0; i < bitGroups.Length; i++) {
-            //     switch (rows[i]) {
-            //         case 1:
+            // Do the conversions to BCD
+            uint ConvertedBits = 0;
+            int BitsIndex;
+            for (int i = 0; i < bitGroups.Length; i++) {    // Iterate through the bitGroups
+                char currentChar;
+                for (int j = 0; j < BCDBits[i].Length; j++) {   // Iterate through the individual bits
+                    currentChar = BCDBits[rows[i] - 1][BCDBits[i].Length - 1 - j];  // Written to go through backwards to easier set the BCD bits
+                    if (currentChar == '0') {
+                        continue;
+                    }
+                    if (currentChar == '1') {
+                        SetBit(ref ConvertedBits, i * 12 + j);
+                    }
 
-            //     }
-            // }
+                    Console.Write(currentChar);
+                    
+                    BitsIndex = DPDBits[i].Length - 1 - Array.IndexOf(DPDBits[rows[i] - 1], currentChar); 
+                    Console.Write(BitsIndex + " ");
+
+                    CopyBit(bitGroups[i], BitsIndex, ref ConvertedBits, i * 12 + j);
+                }
+                Console.WriteLine("   For row " + rows[i]);
+                // DPDBits[rows[i]]
+            }
             
-            return bitGroups[2];
+            return ConvertedBits;
         }
     }
 }
--- C#Zone/DPDLab/obj/Debug/net8.0/DPDLab.AssemblyInfo.cs
+++ C#Zone/DPDLab/obj/Debug/net8.0/DPDLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DPDLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023388e50d6e943f8cbfe44be3a4ead77292aa13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda98e5155c78cb5b2b691bd8c073c92eddee035")]
 [assembly: System.Reflection.AssemblyProductAttribute("DPDLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DPDLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
